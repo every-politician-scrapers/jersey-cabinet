7c105f8ea6de0592717b9ce2ab13e51767fd3b3a
Tony Bowden <tony@tmtm.com>
2022-07-13 09:44:51 +0300

    upgrade scraper

--- sources/official/add-source-name.js
+++ sources/official/add-source-name.js
@@ -1,15 +1,12 @@
-// wb ar add-source-name.js Q97131330-4C4A7FD7-C10F-4D38-B5FD-03F6EADF409A "Vassilios Demetriades" "Deputy Minister of Shipping"
+const fs = require('fs');
+let rawmeta = fs.readFileSync('meta.json');
+let meta = JSON.parse(rawmeta);
 
 module.exports = (guid, name, position) => ({
     guid,
     snaks: {
-      P854: 'https://www.gov.je/Government/HowGovernmentWorks/CouncilMinisters/Pages/CouncilofMinisters.aspx',
-      P1476: {
-        text: 'Council of Ministers',
-        language: 'en',
-      },
+      ...meta.reference,
       P813: new Date().toISOString().split('T')[0],
-      P407: 'Q1860', // language: English
       P1810: name, // named as (Person)
       P1932: position, //stated as (Position)
     }
